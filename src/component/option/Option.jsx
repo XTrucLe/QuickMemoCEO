cc856a4676fe131e1e7b9076adf6e9f588ea13f4
trucle0505 <trucle050503@gmail.com>
2024-03-30 20:57:46 +0700

    first commit

--- src/component/option/Option.jsx
+++ src/component/option/Option.jsx
@@ -4,10 +4,7 @@ import '../option/Option.css'
 const Option = (icon, nameOption, value, color) => {
   return (
     <div className='container w-96 h-60' style={{color:'blue'}}>
-    <div style={{color:'red'}}></div>
-    <div style={{color:'red'}}></div>
-    <div style={{color:'red'}}></div>
-    
+  
     </div>
   )
 }
