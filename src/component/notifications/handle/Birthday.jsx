ad3746e4a04ba82980b068a8871eeb02641e33fe
trucle0505 <trucle050503@gmail.com>
2024-05-13 20:24:46 +0700

    handle birthday notification

---
+++ src/component/notifications/handle/Birthday.jsx
@@ -0,0 +1,42 @@
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+
+function BirthdayNotification({ data }) {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const processedMessages = processBirthdays(data);
+        setMessages(processedMessages);
+    }, [data]);
+
+    function processBirthdays(data) {
+        const today = new Date();
+        const processedMessages = [];
+
+        data.forEach(person => {
+            const birthDate = new Date(person.BIRTH_DATE);
+            const nextBirthday = new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate());
+
+            if (nextBirthday < today) {
+                nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
+            }
+
+            const daysUntilBirthday = Math.ceil((nextBirthday - today) / (1000 * 60 * 60 * 24));
+
+            const birthdayMessage = {
+                title: "Birthday Notification",
+                dateTime: dayjs(nextBirthday).format('ss:mm:HH DD-MM-YYYY'),
+                fullName: `${person.CURRENT_FIRST_NAME} ${person.CURRENT_LAST_NAME}`,
+                message: `${person.CURRENT_FIRST_NAME} ${person.CURRENT_LAST_NAME}'s birthday is in ${daysUntilBirthday} days`
+            };
+
+            processedMessages.push(birthdayMessage);
+        });
+
+        return processedMessages;
+    }
+
+    return messages;
+}
+
+export default BirthdayNotification;
--- src/pages/notification/BirthDays.jsx
+++ src/pages/notification/BirthDays.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Triangle from '../../component/triangle/Triangle'
-import ListCard from '../../component/notifications/ListCard'
+import GetData from '../../api/REST/Get'
+import { notification } from '../../api'
+import BirthdayNotification from '../../component/notifications/handle/Birthday'
+import { Card } from 'antd';
 
 const BirthDays = () => {
-   // set triangle properties
-   const triangle = {
+  const [notiData, setNotiData] = useState([])
+  const newData = GetData({ url: notification.birthDay, dataField: 'data' });
+  const listData = BirthdayNotification({ data: newData })
+
+  useEffect(() => {
+    setNotiData(listData);
+  }, [listData])
+  // set triangle properties
+  const triangle = {
     size: '20px',
     location: '87.5%',
     color: '#b3fce6'
@@ -13,7 +23,13 @@ const BirthDays = () => {
     <div className='grid grid-cols-1 w-full h-auto'>
       <Triangle size={triangle.size} location={triangle.location} color={triangle.color} />{/* small triangle in top notification container */}
       <div className=' w-full h-screen p-3 rounded-xl' style={{ background: '#b3fce6' }}>{/* container of notifiaction */}
-        <ListCard/>
+        {notiData.map((notification, index) => (
+          <Card key={index} title={notification.title}>
+            <p><strong>Name:   </strong>{notification.fullName}</p>
+            <p><strong>Date:   </strong>{notification.dateTime}</p>
+            <p><strong>Message: </strong>{notification.message}</p>
+          </Card>
+        ))}
       </div>
     </div>
   )
